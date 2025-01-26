10cf064ac0b0a8194811649a27e2937e2ae8a955
anas-derk <anas.derk2023@gmail.com>
2025-01-27 02:52:10 +0300

    Fix

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,18 +1,49 @@
 import Carousel from "react-bootstrap/Carousel";
-import TestImage from "../../../public/images/testImage.jpg";
+import SliderImage1 from "../../../public/images/SlideImage1.jpg";
+import SliderImage2 from "../../../public/images/SlideImage2.jpg";
+import SliderImage3 from "../../../public/images/SlideImage3.jpg";
+import SliderImage4 from "../../../public/images/SlideImage4.jpg";
+import SliderImage5 from "../../../public/images/SlideImage5.jpg";
+import SliderImage6 from "../../../public/images/SlideImage6.jpg";
+import SliderImage7 from "../../../public/images/SlideImage7.jpg";
 
 export default function Slider() {
     return (
         <Carousel indicators={false}>
-            <Carousel.Item>
-                <img src={TestImage.src} alt="" className="mw-100" />
-            </Carousel.Item>
-            <Carousel.Item>
-                <img src={TestImage.src} alt="" className="mw-100" />
-            </Carousel.Item>
-            <Carousel.Item>
-                <img src={TestImage.src} alt="" className="mw-100" />
-            </Carousel.Item>
+            {[
+                {
+                    alt: "Slider Image 1",
+                    src: SliderImage1.src,
+                },
+                {
+                    alt: "Slider Image 2",
+                    src: SliderImage2.src,
+                },
+                {
+                    alt: "Slider Image 3",
+                    src: SliderImage3.src,
+                },
+                {
+                    alt: "Slider Image 4",
+                    src: SliderImage4.src,
+                },
+                {
+                    alt: "Slider Image 5",
+                    src: SliderImage5.src,
+                },
+                {
+                    alt: "Slider Image 6",
+                    src: SliderImage6.src,
+                },
+                {
+                    alt: "Slider Image 7",
+                    src: SliderImage7.src,
+                }
+            ].map((item, index) => (
+                <Carousel.Item key={index}>
+                    <img src={item.src} alt={item.alt} className="mw-100" />
+                </Carousel.Item>
+            ))}
         </ Carousel>
     );
 }
