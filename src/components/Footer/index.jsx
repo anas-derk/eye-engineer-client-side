e468fb21a9bf11ea78fbe35f320447863c881eb6
anas-derk <anas.derk2023@gmail.com>
2025-01-27 15:48:00 +0300

    Continue

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,26 +1,15 @@
 import Link from "next/link";
 import { useTranslation } from "react-i18next";
 import { useEffect, useState } from "react";
-import { FaLongArrowAltRight, FaLongArrowAltLeft, FaCcPaypal, FaInstagram, FaTiktok } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
-import { LiaCcVisa } from "react-icons/lia";
-import { FaCcMastercard } from "react-icons/fa";
+import { FaLongArrowAltRight, FaLongArrowAltLeft, FaInstagram, FaTiktok } from "react-icons/fa";
+import { BiLogoMicrosoftTeams } from "react-icons/bi";
 import { IoLogoFacebook } from "react-icons/io";
 import { MdEmail } from "react-icons/md";
-import axios from "axios";
 import { getAnimationSettings, getInitialStateForElementBeforeAnimation, handleSelectUserLanguage } from "../../../public/global_functions/popular";
 import { motion } from "motion/react";
 import WebsiteLogo from "../../../public/images/LogoWithTransparentBackground.png"
 
 export default function Footer() {
-
-    const [email, setEmail] = useState("");
-
-    const [waitMsg, setWaitMsg] = useState("");
-
-    const [errMsg, setErrorMsg] = useState("");
-
-    const [successMsg, setSuccessMsg] = useState("");
 
     const { i18n, t } = useTranslation();
 
@@ -28,38 +17,6 @@ export default function Footer() {
         const userLanguage = localStorage.getItem(process.env.userlanguageFieldNameInLocalStorage);
         handleSelectUserLanguage(userLanguage === "ar" || userLanguage === "en" || userLanguage === "tr" || userLanguage === "de" ? userLanguage : "en", i18n.changeLanguage);
     }, []);
-
-    const handleSubscription = async (e, email) => {
-        try {
-            e.preventDefault();
-            setWaitMsg("Please Wait ...");
-            const result = (await axios.post(`${process.env.BASE_API_URL}/subscriptions/add-new-subscription?language=${i18n.language}`, {
-                email,
-            })).data;
-            setWaitMsg("");
-            if (!result.error) {
-                setSuccessMsg("Success Subscription !!");
-                let errorTimeout = setTimeout(() => {
-                    setSuccessMsg("");
-                    clearTimeout(errorTimeout);
-                }, 5000);
-            } else {
-                setErrorMsg(result.msg);
-                let errorTimeout = setTimeout(() => {
-                    setErrorMsg("");
-                    clearTimeout(errorTimeout);
-                }, 5000);
-            }
-        }
-        catch (err) {
-            setWaitMsg(false);
-            setErrorMsg(err?.message === "Network Error" ? "Network Error" : "Sorry, Something Went Wrong, Please Try Again !");
-            let errorTimeout = setTimeout(() => {
-                setErrorMsg("");
-                clearTimeout(errorTimeout);
-            }, 5000);
-        }
-    }
 
     return (
         <footer className="pt-4 pb-4">
@@ -78,7 +35,6 @@ export default function Footer() {
                         }}
                     >
                         <img src={WebsiteLogo.src} alt={`${process.env.websiteName} Image`} className="mw-100 mb-4" />
-                        {!waitMsg && !successMsg && !errMsg && <Link className="btn orange-btn d-block w-100" href="/">{t("Send Message")}</Link>}
                     </motion.div>
                     <motion.div
                         className="col-xl-3"
@@ -114,23 +70,23 @@ export default function Footer() {
                             </li>
                             <li className="link-item fw-bold mb-3">
                                 {i18n.language !== "ar" ? <FaLongArrowAltRight className="me-2" /> : <FaLongArrowAltLeft className="ms-2" />}
-                                <Link href="/return-and-refund-policy" className="text-dark link-btn">{t("Reserve Office").toUpperCase()}</Link>
+                                <Link href="/reserve-office" className="text-dark link-btn">{t("Reserve Office").toUpperCase()}</Link>
                             </li>
                             <li className="link-item fw-bold mb-3">
                                 {i18n.language !== "ar" ? <FaLongArrowAltRight className="me-2" /> : <FaLongArrowAltLeft className="ms-2" />}
-                                <Link href="/return-and-delivery-sheet" className="text-dark link-btn">{t("Intelligent Translator").toUpperCase()}</Link>
+                                <Link href="/intelligent-translator" className="text-dark link-btn">{t("Intelligent Translator").toUpperCase()}</Link>
                             </li>
                             <li className="link-item fw-bold mb-3">
                                 {i18n.language !== "ar" ? <FaLongArrowAltRight className="me-2" /> : <FaLongArrowAltLeft className="ms-2" />}
-                                <Link href="/return-and-delivery-sheet" className="text-dark link-btn">{t("Privacy Policy").toUpperCase()}</Link>
+                                <Link href="/privacy-policy" className="text-dark link-btn">{t("Privacy Policy").toUpperCase()}</Link>
                             </li>
                             <li className="link-item fw-bold mb-3">
                                 {i18n.language !== "ar" ? <FaLongArrowAltRight className="me-2" /> : <FaLongArrowAltLeft className="ms-2" />}
-                                <Link href="/return-and-delivery-sheet" className="text-dark link-btn">{t("Articles Policy").toUpperCase()}</Link>
+                                <Link href="/articles-policy" className="text-dark link-btn">{t("Articles Policy").toUpperCase()}</Link>
                             </li>
                             <li className="link-item fw-bold">
                                 {i18n.language !== "ar" ? <FaLongArrowAltRight className="me-2" /> : <FaLongArrowAltLeft className="ms-2" />}
-                                <Link href="/return-and-delivery-sheet" className="text-dark link-btn">{t("Questions Policy").toUpperCase()}</Link>
+                                <Link href="/questions-policy" className="text-dark link-btn">{t("Questions Policy").toUpperCase()}</Link>
                             </li>
                         </ul>
                     </motion.div>
@@ -191,25 +147,29 @@ export default function Footer() {
                         >
                             <li className="link-item fw-bold mb-3">
                                 <IoLogoFacebook className={`${i18n.language !== "ar" ? "me-2" : "ms-2"}`} />
-                                <Link href="https://www.facebook.com/AsfourInternational?mibextid=ZbWKwL" target="_blank" className="text-dark link-btn">{t("Facebook").toUpperCase()}</Link>
+                                <Link href="https://www.facebook.com/eyeengineer?mibextid=ZbWKwL" target="_blank" className="text-dark link-btn">{t("Facebook").toUpperCase()}</Link>
                             </li>
                             <li className="link-item fw-bold mb-3">
                                 <FaInstagram className={`${i18n.language !== "ar" ? "me-2" : "ms-2"}`} />
-                                <Link href="https://www.instagram.com/ubuyblues" target="_blank" className="text-dark link-btn">{t("Instagram").toUpperCase()}</Link>
+                                <Link href="https://www.instagram.com/eyeengineer?igsh=MzRlODBiNWFlZA==" target="_blank" className="text-dark link-btn">{t("Instagram").toUpperCase()}</Link>
                             </li>
                             <li className="link-item fw-bold mb-3">
                                 <FaTiktok className={`${i18n.language !== "ar" ? "me-2" : "ms-2"}`} />
-                                <Link href="https://www.tiktok.com/@ubuyblues" target="_blank" className="text-dark link-btn">{t("Tiktok").toUpperCase()}</Link>
+                                <Link href="https://www.tiktok.com/@cirat.co?_t=8oZFsy0KQ2z&_r=1" target="_blank" className="text-dark link-btn">{t("Tiktok").toUpperCase()}</Link>
+                            </li>
+                            <li className="link-item fw-bold mb-3">
+                                <BiLogoMicrosoftTeams className={`${i18n.language !== "ar" ? "me-2" : "ms-2"}`} />
+                                <Link href="https://teams.live.com/l/community/FEAG3TGynF-xu-I6AI" target="_blank" className="text-dark link-btn">{t("Tiktok").toUpperCase()}</Link>
                             </li>
                             <li className="link-item fw-bold mb-3">
                                 <MdEmail className={`${i18n.language !== "ar" ? "me-2" : "ms-2"}`} />
-                                <Link href="mailto:info@ubuyblues.com" className="text-dark link-btn">{t("Email").toUpperCase()}</Link>
+                                <Link href="mailto:info@eyeengineer.com" className="text-dark link-btn">{t("Email").toUpperCase()}</Link>
                             </li>
                         </ul>
                     </motion.div>
                 </div>
                 <p className="mb-0 text-center fw-bold">
-                    {t("All Rights Reserved For")} <Link href="/" className="text-danger">{process.env.WebsiteName}</Link>
+                    {t("All Rights Reserved For")} <Link href="/" className="website-link">{process.env.websiteName}</Link>
                 </p>
             </div>
         </footer>
