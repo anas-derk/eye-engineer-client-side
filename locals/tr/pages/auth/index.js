11fa162df3fac7c7f7cb50ed3bae03d7833af2fb
anas derk <anas.derk@gmail.com>
2025-07-03 12:41:31 +0100

    Continue

---
+++ locals/tr/pages/auth/index.js
@@ -0,0 +1,11 @@
+import accountVerificationTranslations from "./account-verification";
+import forgetPasswordTranslations from "./forget-password";
+import loginTranslations from "./login";
+import signUpTranslations from "./sign-up";
+
+export default {
+    accountVerificationTranslations,
+    forgetPasswordTranslations,
+    loginTranslations,
+    signUpTranslations
+}
--- locals/tr/pages/index.js
+++ locals/tr/pages/index.js
@@ -1,19 +1,13 @@
+import authTranslations from "./auth";
 import aboutUsTranslations from "./about-us";
-import accountVerificationTranslations from "./account-verification";
 import contactUsTranslations from "./contact-us";
 import dashboardTranslations from "./dashboard";
-import forgetPasswordTranslations from "./forget-password";
-import loginTranslations from "./login";
 import profileTranslations from "./profile";
-import signUpTranslations from "./sign-up";
 
 export default {
+    ...authTranslations,
     ...aboutUsTranslations,
-    ...accountVerificationTranslations,
     ...contactUsTranslations,
     ...dashboardTranslations,
-    ...forgetPasswordTranslations,
-    ...loginTranslations,
     ...profileTranslations,
-    ...signUpTranslations
 }
