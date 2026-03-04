800390da56dd97e2c0ab55ad38cd2127def50da9
anas derk <anas.derk@gmail.com>
2026-03-04 23:44:34 +0300

    Add Some Of Translations

--- locals/de/components/index.js
+++ locals/de/components/index.js
@@ -1,10 +1,12 @@
 import capabiltesTranslations from "./capabilites";
+import changeOfficeStatusTranslations from "./change-office-status";
 import confirmDeleteTranslations from "./confirm-delete";
 import footerTranslations from "./footer";
 import headerTranslations from "./header";
 
 export default {
     ...capabiltesTranslations,
+    ...changeOfficeStatusTranslations,
     ...confirmDeleteTranslations,
     ...footerTranslations,
     ...headerTranslations
